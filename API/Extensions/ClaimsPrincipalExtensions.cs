270e3511aceae418247ee7abba3aad7f3b95057b
justanoldaccount <divyanshu.sharma@citrix.com>
2022-01-02 12:26:18 -0600

    added photo uplaod and deletion

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,15 +1,11 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Security.Claims;
-// using System.Threading.Tasks;
+using System.Security.Claims;
 
-// namespace API.Extensions
-// {
-//     public static class ClaimsPrincipalExtensions
-//     {
-//         public static string GetUsername(this ClaimsPrincipal user) {
-
-//         }
-//     }
-// }
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user) {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
